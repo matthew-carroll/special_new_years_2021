1210c6834a567610a3953aa1ec611b5e9e02b533
Matt Carroll <matthew-carroll@users.noreply.github.com>
2020-12-18 20:28:45 -0800

    Step 1: Create night landscape.

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,65 @@ class MyApp extends StatelessWidget {
 class NewYearsCountdownScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Landscape(),
+    );
+  }
+}
+
+class Landscape extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        _buildSky(),
+        _buildStars(),
+        _buildMountains(),
+      ],
+    );
+  }
+
+  Widget _buildSky() {
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        gradient: _buildGradient(),
+      ),
+      child: SizedBox.expand(),
+    );
+  }
+
+  LinearGradient _buildGradient() {
+    return LinearGradient(
+      colors: [
+        const Color(0xFF19142a),
+        const Color(0xFF3f2b87),
+      ],
+      begin: Alignment.topCenter,
+      end: Alignment.bottomCenter,
+    );
+  }
+
+  Widget _buildStars() {
+    return Positioned(
+      left: 0,
+      right: 0,
+      top: 0,
+      child: Image.asset(
+        'assets/stars.png',
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+
+  Widget _buildMountains() {
+    return Positioned(
+      left: 0,
+      right: 0,
+      bottom: 0,
+      child: Image.asset(
+        'assets/mountains_night.png',
+        fit: BoxFit.cover,
+      ),
+    );
   }
 }
